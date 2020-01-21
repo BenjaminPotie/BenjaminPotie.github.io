a75f31b793f8f20ad1cd82e391f2af0a4cd9f67d
Benjamin Potie <benjaminpotie@MacBook-Pro-de-Benjamin-3.local>
2020-01-21 10:25:18 +0100

    Change 21/01/20

--- Javascript/main.js
+++ Javascript/main.js
@@ -5,35 +5,9 @@ document.addEventListener("mousemove",followMouse);
 function followMouse(e){
     let x=e.x, y=e.y;
     follow.style.transform=`matrix(1,0,0,1,${x},${y})`;
-    if(e.target.nodeName=='A' || e.target.parentNode.nodeName=='A' || e.target.nodeName=='.TXT'){
+    if(e.target.nodeName=='A' || e.target.parentNode.nodeName=='A'){
         follow.classList.add('circleH');
     }else{
         follow.classList.remove('circleH');
     }
 }
-
-/* -------------------------------- IMG + TXT ------------------------------- */
-// var img = document.querySelectorAll(".container a");
-
-// for (var i=0; i<img.length; i++) {
-//     img[i].addEventListener("mouseenter", myMouseOver);
-//     img[i].addEventListener("mouseleave", myMouseOut);
-// }
-
-// function myMouseOver (event) {
-//     event.stopPropagation();
-//     var image = event.target.querySelector("img");
-//     image.style.opacity = "0.05";
-
-//     var texte = event.target.querySelector(".txt");
-//     texte.style.display ="block";
-// }
-
-// function myMouseOut (event) {
-//     event.stopPropagation();
-//     var image = event.target.querySelector("img");
-//     image.style.opacity = "1";
-
-//     var texte = event.target.querySelector(".txt");
-//     texte.style.display ="none";
-// }
