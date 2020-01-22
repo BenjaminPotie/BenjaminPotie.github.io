662e1af94e2132dced5e98ef0b3cd70dbcdfa012
Benjamin Potie <benjaminpotie@MacBook-Pro-de-Benjamin-3.local>
2020-01-22 16:43:54 +0100

    Change

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,14 +1,29 @@
 const $ = document.querySelector.bind(document);
 /* --------------------------------- CURSOR --------------------------------- */
 const follow = document.querySelector(".circle");
+follow.style.display = "none";
 
-document.addEventListener("mousemove",followMouse);
 function followMouse(e){
     let x=e.x, y=e.y;
     follow.style.transform=`matrix(1,0,0,1,${x},${y})`;
     if(e.target.nodeName=='A' || e.target.parentNode.nodeName=='A'){
         follow.classList.add('circleH');
-    }else{
+    }
+    else {
         follow.classList.remove('circleH');
     }
 }
+
+window.onresize = onResize;
+
+function onResize(){
+if (window.innerWidth>1024) {
+    follow.style.display = "block";
+document.addEventListener("mousemove",followMouse);
+} else {
+    follow.style.display = "none";
+    document.removeEventListener("mousemove",followMouse);
+    
+}
+}
+onResize();
